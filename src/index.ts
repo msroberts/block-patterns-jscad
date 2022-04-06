f600d98829340b93cabb18cf1e53e5d386ce46e2
Madeleine Roberts <msroberts@users.noreply.github.com>
2022-04-06 17:48:43 -0400

    Use design parameters

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import { primitives } from "@jscad/modeling";
+import { BlockParameters, DesignParameter } from "./types/params";
 
 const { star } = primitives;
 
-export const main = () => {
-  const newStar = star({ outerRadius: 6, center: [2, 3] });
+export const main = ({ param1 }: BlockParameters) => {
+  const newStar = star({ outerRadius: param1, center: [2, 3] });
 
   return [newStar];
+};
+
+export const getParameterDefinitions = () => {
+  const parameters: DesignParameter[] = [
+    {
+      name: "param1",
+      type: "int",
+      initial: 10,
+      caption: "Parameter 1",
+    },
+  ];
+
+  return parameters;
 };
---
+++ src/types/params.ts
@@ -0,0 +1,10 @@
+export interface BlockParameters {
+  param1: number;
+}
+
+export interface DesignParameter {
+  name: string;
+  type: string;
+  initial: number;
+  caption: string;
+}
